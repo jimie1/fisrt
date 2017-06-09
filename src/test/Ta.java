6a4e806e480cbb11aa7c76e3a23c3da56e4a97f7
Administrator <Administrator@A-201702161532>
2017-06-09 16:27:57 +0800

    22222

---
+++ src/test/Ta.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Ta {
+		public static void main(String[] args) {
+			Ta ta=new Ta();
+			System.out.println("提交至github");
+		}
+		public void print(){
+			System.out.println("提交三次");
+		}
+		public int sum(int x,int y){
+			
+			return x+y;
+		}
+}
